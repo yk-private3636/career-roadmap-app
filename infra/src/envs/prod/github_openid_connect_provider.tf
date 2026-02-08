6b6d307a78964034526815d7d2ee215f025fc94f
yk-private3636 <hsnysk318@gmail.com>
2026-02-08 18:31:39 +0900

    GithubActions AWS OIDC認証設定

---
+++ infra/src/envs/prod/github_openid_connect_provider.tf
@@ -0,0 +1,7 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+}
--- infra/src/envs/prod/terraform_role.tf
+++ infra/src/envs/prod/terraform_role.tf
@@ -7,19 +7,40 @@ module "terraform_role" {
 
 data "aws_iam_policy_document" "terraform_assume_role_policy" {
   statement {
+    effect  = "Allow"
     actions = ["sts:AssumeRole"]
-
     principals {
       type = "AWS"
       identifiers = [
         "arn:aws:iam::${var.account_id}:user/career-roadmap-app-terraform",
+        "arn:aws:sts::${var.account_id}:assumed-role/${local.terraform_role_name}/GitHubActions"
       ]
     }
-
     condition {
       test     = "Bool"
       variable = "aws:MultiFactorAuthPresent"
       values   = ["true"]
+    }
+  }
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type = "Federated"
+      identifiers = [
+        aws_iam_openid_connect_provider.github.arn
+      ]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:yk-private3636/career-roadmap-app:*"]
     }
   }
 }
